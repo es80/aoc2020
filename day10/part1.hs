9d928a5c4395ec3843f00330ad4661e1a325100a
es80 <elliotts@apadmi.com>
2020-12-10 22:13:25 +0000

    day10

---
+++ day10/part1.hs
@@ -0,0 +1,25 @@
+import           System.Environment
+import           System.IO
+import           Data.List
+
+gaps :: [Int] -> [Int]
+gaps []           = []
+gaps [x         ] = []
+gaps (x : y : ys) = y - x : gaps (y : ys)
+
+getMultiple :: [Int] -> Int
+getMultiple nums =
+  let numGaps = gaps (0 : sort nums) ++ [3]
+      counts  = \x -> length (filter (== x) numGaps)
+  in  counts 1 * counts 3
+
+parseInput :: String -> [Int]
+parseInput fileContent = map (read :: String -> Int) $ lines fileContent
+
+main :: IO ()
+main = do
+  args     <- getArgs
+  inHandle <- openFile (head args) ReadMode
+  contents <- hGetContents inHandle
+  print $ getMultiple $ parseInput contents
+
